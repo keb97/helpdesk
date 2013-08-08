06ba4cee9fbf4efcc0026a4a69870fc095d9ec8d
Kathryn <keb97@cornell.edu>
2013-08-08 15:15:29 -0700

    now saving the ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,5 +4,6 @@ class Ticket < ActiveRecord::Base
   def self.create_from_postmark(mitt)
     @ticket = Ticket.find(mitt.subject.to_i)
     @ticket.solution = mitt.text_body
+    @ticket.save
   end
 end
