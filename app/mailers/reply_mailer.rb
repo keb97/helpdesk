59109b42097bfd667859214994b3fba3b7ecce4c
Kathryn <keb97@cornell.edu>
2013-08-07 19:50:20 -0700

    trying to send an email to the app

--- app/mailers/reply_mailer.rb
+++ app/mailers/reply_mailer.rb
@@ -3,6 +3,11 @@ class ReplyMailer < ActionMailer::Base
 
   def question_email(ticket)
     @ticket = ticket
-    mail(to: "mewzik@gmail.com", subject: "There are questions.")
+    mail(to: "mewzik@gmail.com", subject: "There are questions.", reply_to: "233bf990395644b63ab70ce6a1a3ea7d@inbound.postmarkapp.com
+")
   end
+
+  def receive(email)
+    @ticket = Ticket.find_by_id(email.subject)
+    @ticket.solution = email.body
 end
