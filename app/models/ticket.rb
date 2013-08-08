289a2027598bd5d3ae4e0cb3d6ba1ec5cecc8c7b
Kathryn <keb97@cornell.edu>
2013-08-08 14:13:06 -0700

    trying to receive email

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,7 +2,7 @@ class Ticket < ActiveRecord::Base
   attr_accessible :problem, :solution
 
   def self.create_from_postmark(mitt)
-    @ticket = Ticket.find(mitt.subject)
+    @ticket = Ticket.find(mitt.subject.to_i)
     @ticket.solution = mitt.text_body
   end
 end
