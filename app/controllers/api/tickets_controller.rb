eabf67e513ba792fdcafe8f9f7f4d4e10e1d6cd2
Kathryn <keb97@cornell.edu>
2013-08-08 14:18:41 -0700

    skipping authenticity token filter

--- app/controllers/api/tickets_controller.rb
+++ app/controllers/api/tickets_controller.rb
@@ -1,4 +1,5 @@
 class Api::TicketsController < ApplicationController
+skip_before_filter :verify_authenticity_token
 
   def create
     Ticket.create_from_postmark(Postmark::Mitt.new(request.body.read))
