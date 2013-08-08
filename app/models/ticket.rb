ac8a08f96887d17edad6376adc631eee4e478481
Kathryn <keb97@cornell.edu>
2013-08-08 13:23:47 -0700

    changed to specify which ticket we're talking about in api namespace ticket_controller create

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,7 +2,7 @@ class Ticket < ActiveRecord::Base
   attr_accessible :problem, :solution
 
   def self.create_from_postmark(mitt)
-    #Ticket.find(mitt.subject #is the ticket id)
-    ticket.solution = mitt.text_body
+    @ticket = Ticket.find(mitt.subject)
+    @ticket.solution = mitt.text_body
   end
 end
