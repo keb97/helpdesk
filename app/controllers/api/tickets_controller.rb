b1dacb91d004720b0efe66a71461cdf90ee96b37
Kathryn <keb97@cornell.edu>
2013-08-08 14:33:19 -0700

    added something to show after created

--- app/controllers/api/tickets_controller.rb
+++ app/controllers/api/tickets_controller.rb
@@ -3,6 +3,7 @@ skip_before_filter :verify_authenticity_token
 
   def create
     Ticket.create_from_postmark(Postmark::Mitt.new(request.body.read))
+    render :text => "You created!", status: :created
   end
 end
 
